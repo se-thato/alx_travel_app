333fad1d87677b273135a140518cfb23fcc135c7
se-thato <thatoselepe80@gmail.com>
2025-05-24 02:14:28 +0200

    changes

--- alx_travel_app/alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/alx_travel_app/settings.py
@@ -1,4 +1,14 @@
 from pathlib import Path
+import environ
+import os
+
+
+# Initialize environment variables
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +39,8 @@ INSTALLED_APPS = [
     'listings',
     'rest_framework',
     'corsheaders',
+    'drf_yasg',
+    
 ]
 
 MIDDLEWARE = [
@@ -66,8 +78,12 @@ WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ev_charging_db',
+        'USER': 'root',
+        'PASSWORD': 'theplanetisflat',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -112,3 +128,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+        },
+    },
+    'USE_SESSION_AUTH': False,
+}
